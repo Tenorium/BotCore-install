9e2a2e8980dd000b9d357cf1a518bbcb4d6856d0
Artem S <admin@tenorium.net>
2022-07-02 21:38:13 +0300

    Updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git init
-git remote add origin https://github.com/Tenorium/BotCore.git
+git remote -f add origin https://github.com/Tenorium/BotCore.git
 git fetch origin
-git checkout -b master --track origin/master
+git checkout -f -b master --track origin/master
 git reset origin/master
